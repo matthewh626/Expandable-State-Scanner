31bbc666051a59067465a8abf288bbd63893973d
matthew <matthew.h626@gmail.com>
2025-09-21 15:38:26 +0100

    finished getLocalHosts module this module will scan for and generate stub descriptor files for the local network and local hosts, refreshes the arp cashe as an unavoidable side effect

--- Modules/getLocalHosts.sh
+++ Modules/getLocalHosts.sh
@@ -23,20 +23,26 @@ arp -n | grep -v "incomplete" > ../Temp/arpCashe
 tail -n +2 ../Temp/arpCashe > ../Temp/arpCashe2
 mv ../Temp/arpCashe2 ../Temp/arpCashe
 echo "Checking if network is known..."
-gatewayIp=$(ip route show | grep 'default' | awk "{print $3}")
-gatewayMac=$(cat ../Temp/arpCashe | grep "$gatewayIp" | grep -E -o "[[:xdigit:]:]{17}")
-if [ ! -f "../Networks/$gatewayMac"]; then
+gatewayIp=$(ip route show | grep 'default' | awk '{print $3}')
+gatewayMac=$(cat ../Temp/arpCashe | grep "$gatewayIp " | grep -E -o "[[:xdigit:]:]{17}")
+if [ ! -f "../Networks/$gatewayMac" ]; then
 	echo "New Network, generating profile stub.."
 	echo "Gateway Ip: $gatewayIp" > "../Networks/$gatewayMac"
 	echo "Public Ip: $(curl api.ipify.org)" >> "../Networks/$gatewayMac"
 	echo "Arp scan:" >> "../Networks/$gatewayMac"
 	cat "../Temp/arpCashe" >> "../Networks/$gatewayMac"
-
-#echo "Checking for Device files..."
-#cat ../Temp/arpCashe | while read line
-#do
-#	mac = $(echo "$line" | grep -o -i "[0-9a-f]\{12\}"
-#	ip = $(echo "$line" | grep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
-#	if [ ! -f "../Devices/$mac"]; then
-#		echo "last seen at "
-#
+else
+	echo "Network known, moving on..."
+fi
+echo "Checking for Device files..."
+cat ../Temp/arpCashe | while read line
+do
+	mac=$(echo "$line" | grep -E -o "[[:xdigit:]:]{17}")
+	ip=$(echo "$line" | grep -E -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+	if [ ! -f "../Devices/$mac" ]; then
+		echo "New device $mac found, regestering..."
+		echo "last seen at $ip" > "../Devices/$mac"
+	else
+		echo "Device already seen before, skipping..."
+	fi
+done
